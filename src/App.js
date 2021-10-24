42be4b87c7b90692c41551c4b36801a61d2a48a2
AngelaTaylor87 <82851741+AngelaTaylor87@users.noreply.github.com>
2021-10-24 16:15:30 -0500

    added imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 //import { div } from "prelude-ls";
 import React from "react";
+import { Container } from 'react-bootstrap'
 //import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 // const client = new ApolloClient({
 //   uri: "/graphql",
@@ -12,9 +13,15 @@ import React from "react";
 function App() {
   return (
     <> 
+    <Header />
+    <main>
+      <Container>
    <h1> Welcome to Aston Jeremiah's online boutique </h1>
+   </Container>
+   </main>
+   <Footer />
     </>
-  );
+  )
 }
 
 export default App;
