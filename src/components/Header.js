d41d1f495ac7f3008cbe0b02edbc8d08d43484fa
AngelaTaylor87 <82851741+AngelaTaylor87@users.noreply.github.com>
2021-10-24 16:43:19 -0500

    added nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
     return (
         <header>
-            Header
+            <Navbar bg="light" expand="lg" >
+  <Container>
+    <Navbar.Brand href="/">Aston Jeremiah's</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="/cart">Cart</Nav.Link>
+        <Nav.Link href="/Login">Sign in</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
         </header>
     )
 }
