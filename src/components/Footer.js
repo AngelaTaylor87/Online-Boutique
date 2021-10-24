3a70365936add745c09224376b3693ec28ccdd89
AngelaTaylor87 <82851741+AngelaTaylor87@users.noreply.github.com>
2021-10-24 16:21:37 -0500

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
     return (
-        <footer>
-            Footer
-        </footer>
+        <Container>
+            <Row>
+                <Col className='text-center py-3'> Copyright &copy; Aston Jeremiah's </Col>
+            </Row>
+        </Container>
     )
 }
 
