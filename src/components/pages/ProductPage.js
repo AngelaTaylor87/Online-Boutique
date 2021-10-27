8dc94566c64da9de94a0b144eb5843c49aa07779
AngelaTaylor87 <82851741+AngelaTaylor87@users.noreply.github.com>
2021-10-26 19:03:30 -0500

    added back btn

--- src/components/pages/ProductPage.js
+++ src/components/pages/ProductPage.js
@@ -1,10 +1,18 @@
-import React from 'react'
 
-const ProductPage = () => {
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGoup, Card, Button } from 'react-bootstrap'
+import Rating from '../Rating'
+import products from '../../products'
+
+const ProductPage = ({match}) => {
+    const product = products.find(p => p.id === match.params.id)
     return (
-        <div>
-            Product
-        </div>
+        <>
+        <Link className='btn btn-dark my-3' to='/'> Back
+            </Link>
+            
+        </>
     )
 }
 
