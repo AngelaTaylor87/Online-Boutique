5496b9baa2760935f71893db4b1bbada6c7d0501
AngelaTaylor87 <82851741+AngelaTaylor87@users.noreply.github.com>
2021-10-26 21:09:03 -0500

    price and descr

--- src/components/pages/ProductPage.js
+++ src/components/pages/ProductPage.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGoup, Card, Button, ListGroup } from 'react-bootstrap'
-//import Rating from '../Rating'
+import Rating from '../Rating'
 import products from '../../products'
 
 const ProductPage = ({ match }) => {
     const product = products.find(p => p._id == match.params.id)
     console.log('products')
-    return <>
+    return ( <>
         <Link className='btn btn-dark my-3' to='/'> 
         Back
         </Link>
@@ -16,14 +16,24 @@ const ProductPage = ({ match }) => {
                 <Image src={process.env.PUBLIC_URL + product.image} alt={product.name} />
             </Col>
             <Col md={3}>
-                <ListGroup>
+                <ListGroup variant='flush'>
                 <ListGroup.Item >
                     <h2>{product.name}</h2>
                 </ListGroup.Item>
+                <ListGroup.Item>
+                    <Rating value={product.rating} text={`${product.Reviews} reviews`} />
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    Price: ${product.price}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    Description: {product.description}
+                    </ListGroup.Item>
                 </ListGroup>
             </Col>
         </Row>
     </>
+    )
 }
 
 export default ProductPage
